f933704db20e047919cfc720577c2109d4ee8073
marcschul <marcschulm@gmail.com>
2021-10-11 21:50:42 +0000

    refactor getAllReservations using SQL queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -94,7 +94,6 @@ exports.getUserWithId = getUserWithId;
 // }
 // ------------------------------------------------------------------
 const addUser = function(user) {
-  user.password = 'password';
 
   return pool
     .query(`
@@ -113,8 +112,30 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
+// const getAllReservations = function(guest_id, limit = 10) {
+  // return getAllProperties(null, 2);
+// }
+// -----------------------------------------------------------------
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  console.log(guest_id);
+  
+  return pool
+    .query(`
+    SELECT thumbnail_photo_url,properties.id,title,start_date, end_date,cost_per_night,number_of_bathrooms,number_of_bedrooms,parking_spaces, avg(rating)::NUMERIC as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    AND reservations.end_date < now()::date
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2
+    `, [guest_id, limit])
+    .then((res) => {
+        console.log(res);
+        return res.rows;
+      })
+    .catch((err) => err.message);
 }
 exports.getAllReservations = getAllReservations;
 
